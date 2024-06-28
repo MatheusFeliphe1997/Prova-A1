10d4b1f51ad73015a854e88aa568a94ce76b58c2
MATHEUS <matheus.feliphe@hotmail.com.br>
2024-06-28 12:10:28 -0300

    quase nada
    tentar algo

--- prova-app/src/components/tarefa-alterar.tsx
+++ prova-app/src/components/tarefa-alterar.tsx
@@ -5,4 +5,16 @@ import { Tarefa } from "../models/Tarefa";
 function TarefaAlterar(){
 
     const navigate = useNavigate();
+    useEffect(() => {
+        if (id) {
+          fetch(`http://localhost:5000/api/produto/buscar/${id}`)
+            .then((resposta) => resposta.json())
+            .then((produto: Produto) => {
+              setNome(produto.nome);
+              setDescricao(produto.descricao);
+              setQuantidade(produto.quantidade.toString());
+              setValor(produto.valor.toString());
+            });
+        }
+      }, []);
 }
